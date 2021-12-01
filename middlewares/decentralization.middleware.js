12764ee228151eab74f81cbfd0994129f4ceb42d
vinhphan812 <vinhphan812@gmail.com>
2021-12-01 17:25:28 +0700

    update set menu for user, manager, admin

--- middlewares/decentralization.middleware.js
+++ middlewares/decentralization.middleware.js
@@ -1,19 +1,27 @@
 const User = require("../model/user.model");
 
 const lvlPerms = { admin: 3, manager: 2, user: 1 };
+const menu = { admin: [], manager: [{ title: "Manager", url: "/manager" }] };
 
 module.exports = {
 	decentralization: (perms) => {
 		return async (req, res, next) => {
 			const userId = req.signedCookies.userId;
+			let userPerms = "user";
 
-			const { permission: userPerms } = await User.findOne({
-				_id: userId,
-			});
+			if (userId)
+				userPerms = (
+					await User.findOne({
+						_id: userId,
+					})
+				).permission;
 
-			if (lvlPerms[userPerms] >= lvlPerms[perms]) return next();
+			if (lvlPerms[userPerms] < lvlPerms[perms])
+				return res.status(404).render("error/permission");
 
-			res.status(404).render("error/permission");
+			res.locals.menu = menu[userPerms];
+
+			next();
 		};
 	},
 };

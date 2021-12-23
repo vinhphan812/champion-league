a4588f2e36e777a5375f6a3feb151d510c978826
vinhphan812 <vinhphan812@gmail.com>
2021-12-23 23:49:54 +0700

    delete referee for manager page

--- public/js/manager.js
+++ public/js/manager.js
@@ -2,6 +2,7 @@ window.onload = async () => {
 	$(".league .delete").click(deleteLeague);
 	$(".donors .delete").click(deleteDonor);
 	$("#teams .delete").click(deleteTeam);
+	$("#referees .delete").click(deleteReferees);
 };
 
 function deleteLeague() {
@@ -44,6 +45,22 @@ function deleteDonor() {
 	confirmDelete("Nhà tài trợ " + name, async () => {
 		const { success, message } = await (
 			await fetch("/api/donors/" + id, { method: "DELETE" })
+		).json();
+		if (success) {
+			Alert("Success", message, "OK", "success");
+		}
+		parent.remove();
+	});
+}
+
+function deleteReferees() {
+	const parent = this.parentNode.parentNode;
+	const { id } = parent;
+	const name = parent.querySelector(":nth-child(2)").innerText;
+
+	confirmDelete("Trọng tài " + name, async () => {
+		const { success, message } = await (
+			await fetch("/api/referees/" + id, { method: "DELETE" })
 		).json();
 		if (success) {
 			Alert("Success", message, "OK", "success");

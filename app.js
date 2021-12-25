f1cd2c557bf304631467b61ada9c002b26b0308c
vinhphan812 <vinhphan812@gmail.com>
2021-12-25 13:45:47 +0700

    update port host => 5000

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require("express"),
 mongoose.connect(process.env.URL_DB);
 
 // config port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // init app
 const app = express();

4c21b524e3f2a71d3f16cce4f87fb9c9fa114786
vinhphan812 <vinhphan812@gmail.com>
2022-01-01 00:28:51 +0700

    init match route, view, controller

--- Controllers/league.controller.js
+++ Controllers/league.controller.js
@@ -1,4 +1,5 @@
-const League = require("../model/league.model");
+const League = require("../model/league.model"),
+	Match = require("../model/match.model");
 
 module.exports = {
 	getLeaguePage: async (req, res, next) => {
@@ -18,13 +19,31 @@ module.exports = {
 
 		if (league.length != 24) return res.render("error/404");
 
-		const data = await League.findOne({ _id: league }, { __v: 0 });
+		const data = await League.findOne({ _id: league });
 
-		if (!data) return res.status(404).json(err);
+		if (!data) return res.render("error/404");
 
 		res.locals.league = data;
 		next();
 	},
-	getMatch: async (req, res, next) => {},
-	updateMatch: async (req, res, next) => {},
+	matchMiddleware: async (req, res, next) => {
+		const { match } = req.params;
+
+		if (match.length != 24) return res.render("error/404");
+
+		const data = await Match.findOne({ _id: match });
+
+		if (!data) return res.render("error/404");
+
+		res.locals.match = data;
+		next();
+	},
+
+	getMatch: async (req, res, next) => {
+		const { match } = res.locals;
+		res.render("match");
+	},
+	updateMatch: async (req, res, next) => {
+		const { match } = res.locals;
+	},
 };
--- routers/league.route.js
+++ routers/league.route.js
@@ -9,8 +9,10 @@ router.use(ctrler.leagueMiddleware);
 router.get("/", ctrler.getLeaguePage);
 
 // matchs
-router.route("/matchs/:match", ctrler.getMatch);
+// matchs middleware
+router.use("/matchs/:match", ctrler.matchMiddleware);
 
-router.route("/matchs/:matchs/update", ctrler.updateMatch);
+router.get("/matchs/:match", ctrler.getMatch);
+router.post("/matchs/:match/update", ctrler.updateMatch);
 
 module.exports = router;
